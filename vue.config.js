dbd8aa83774eb03048cb19cb77c868d751024cf0
dawn <dawnflyingnow@qq.com>
2020-09-08 10:33:42 +0800

    跨域配置

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    devServer: {
-        host:"outbreak-screen.vercel.app",
-        port:8081,
-        https:true,
-        open:true,
-          proxy: {
-            "/api": {
-              target:"https://www.maomin.club/", // 目标代理接口地址
-            //   secure: true,
-              changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
-              // ws: true, // 是否启用websockets
-              pathRewrite: {
-                "^/api": "" 
-              }
-            }
-          }
-    }    
+  devServer: {
+    host: "https://outbreak-screen.vercel.app",
+    port: 8081,
+    https: true,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "https://www.maomin.club/", // 目标代理接口地址
+        //   secure: true,
+        changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
+        // ws: true, // 是否启用websockets
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    }
+  }
 }
